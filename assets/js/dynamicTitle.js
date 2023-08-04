fcc19539f454ac36567938626e3ea289344be943
Mustansir Sabir <mustansirsabir@gmail.com>
2023-08-04 12:02:15 +0530

    removed attention message from page title

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -1,20 +1,22 @@
 window.onload = function () {
-  const favicon = document.getElementById('favicon')
-  const pageTitle = document.title
-  const attentionMessage = 'Come back'
+  const favicon = document.getElementById('favicon');
+  const pageTitle = document.title;
+  const attentionMessage = 'Come back';
 
   document.addEventListener('visibilitychange', function (e) {
-    const isPageActive = !document.hidden
-    toggle(isPageActive)
-  })
+    const isPageActive = !document.hidden;
+    toggle(isPageActive);
+  });
 
   function toggle(isPageActive) {
-    if (isPageActive) {
-      document.title = pageTitle
-      favicon.href = './assets/images/dp_male.svg'
-    } else {
-      document.title = attentionMessage
-      favicon.href = './assets/images/folded.png'
-    }
+    document.title = pageTitle;
+    favicon.href = './assets/images/dp_male.svg';
+    // if (isPageActive) {
+    //   document.title = pageTitle
+    //   favicon.href = './assets/images/dp_male.svg'
+    // } else {
+    //   document.title = attentionMessage
+    //   favicon.href = './assets/images/folded.png'
+    // }
   }
-}
+};
